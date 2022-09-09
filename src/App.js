1568413703daf4a5f3baed7513727d3c3c84c538
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-09 15:14:20 +0300

    Finished exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [weight, setWeight] = useState();
+  const [intensity, setIntensity] = useState(0);
+  const [sex, setSex] = useState();
+  const [calories, setCalories] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (sex==="male") {
+      const result = (879 + 10.2*weight)*intensity
+      setCalories(result)
+    }
+    if (sex==="female") {
+      const result = (795 + 7.18*weight)*intensity
+      setCalories(result)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Calorie Calculator</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="">Weight</label>
+          <input type="number" onChange={e => setWeight(e.target.value)} />
+        </div>
+        <div>
+          <label>Intensity</label>
+          <select name="intensity" onChange={e => setIntensity(e.target.value)}>
+            <option value="1.3">Light</option>
+            <option value="1.5">Usual</option>
+            <option value="1.7">Moderate</option>
+            <option value="2">Hard</option>
+            <option value="2.2">Very Hard</option>
+          </select>
+        </div>
+        <div>
+          <label>Sex</label>
+          <input type="radio" id="maleInput" value="male" name="sex" onChange={e => setSex(e.target.value)}/><label for="maleInput">Male</label>
+          <input type="radio" id="femaleInput" value="female" name="sex" onChange={e => setSex(e.target.value)}/><label for="femaleInput">Female</label>
+        </div>
+        <div>
+          <output>{calories.toFixed(0)} Calories</output>
+        </div>
+        <button onClick={handleSubmit}>Calculate</button>
+      </form>
     </div>
   );
 }
